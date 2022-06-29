766a4fdccaf01d29a7a72742ecd083531bc83957
mriganka22 <mrigankahazarika2@gmail.com>
2022-06-29 10:32:57 +0530

    first commit

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,31 @@
+
+import classes from './MealItemForm.module.css';
+
+const MealItemForm = (props) => {
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    props.onAddToCart(1);
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      {/* <Input
+        ref={amountInputRef}
+        label='Amount'
+        input={{
+          id: 'amount',
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
+        }}
+      /> */}
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
